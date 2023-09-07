3b17c77978ac057d07b37b842de0acc5b6f11e18
root <vincentbettoh@gmail.com>
2023-09-07 07:52:09 -0400

    Changes

--- app/src/main/java/com/example/morsecodemessenger/MainActivity.kt
+++ app/src/main/java/com/example/morsecodemessenger/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.morsecodemessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnLogin:MaterialButton=findViewById(R.id.btn_login)
+        val btnSignup:MaterialButton=findViewById(R.id.btn_signup)
+        val email:EditText=findViewById(R.id.etEmail)
+        val password:EditText=findViewById(R.id.edt_password)
+        btnLogin.setOnClickListener {
+            handleLogin(email.text.toString(),password.text.toString())
+        }
+    }
+
+    private fun handleLogin(email: String, password: String) {
+        val auth=FirebaseAuth.getInstance()
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+            if(it.isSuccessful)
+            {
+                Toast.makeText(this,"Login",Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this,"Error logging in",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
